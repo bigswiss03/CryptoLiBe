fb485de9c2bee64b97ca02cf9f66cf93cd4e25a9
Urel-GitHub <55204770+Urel-GitHub@users.noreply.github.com>
2021-05-30 14:04:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import cv2, pafy
+import cv2
+import pafy
 import numpy as np
-url = "https://www.youtube.com/watch?v=m002DceDBgg"
+import winsound # Demo: segnale sonoro
+url = "https://www.youtube.com/watch?v=5K0uURft3MY"
 source = pafy.new(url).getbest()
 capture = cv2.VideoCapture(source.url)
 while (True):
@@ -9,14 +11,14 @@ while (True):
     if cv2.waitKey(1) & 0xFF == ord('q'):   # Quitta se si preme il tasto "q" (spiegazione: https://stackoverflow.com/a/57691103)
         break
 
-    current_frame_cropped = current_frame[50:500, 300:505,:] # (spiegazione: https://stackoverflow.com/a/58177717/15553356)
+    current_frame_cropped = current_frame[50:500, 450:505,:] # (spiegazione: https://stackoverflow.com/a/58177717/15553356)
 
     # Converto in HSV
     hsv = cv2.cvtColor(current_frame_cropped, cv2.COLOR_BGR2HSV)
 
     # Definisco il range dei colori in HSV da cercare
-    lower_val_buy = np.array([37,42,0])
-    upper_val_buy = np.array([84,255,255])
+    lower_val_buy = np.array([40, 40,40])
+    upper_val_buy = np.array([70,255,255])
 
     lower_val_sell = np.array([0,0,255])
     upper_val_sell = np.array([5,255,255])
@@ -25,18 +27,13 @@ while (True):
     mask_buy = cv2.inRange(hsv, lower_val_buy, upper_val_buy)
     mask_sell = cv2.inRange(hsv, lower_val_sell, upper_val_sell)
 
-    # Se applicando la maschera restano degli elementi (=> esistono delle aree verdi), sum sarà > 0
-    hasGreen = np.sum(mask_buy)
-    if hasGreen > 0:
-        print('Verde trovato!')
-
     # Applico una maschera
     res_buy = cv2.bitwise_and(current_frame_cropped,current_frame_cropped,mask=mask_buy)
     res_sell = cv2.bitwise_and(current_frame_cropped,current_frame_cropped,mask=mask_sell)
 
     # Faccio qualche operazione (erosione, dilatazione) per evidenziare meglio l'area rossa "Sell"
     kernel = np.ones((2,2), np.uint8)
-    res_sell = cv2.erode(res_sell, kernel, iterations=1)
+    res_sell = cv2.erode(res_sell, kernel, iterations=2)
     res_sell = cv2.dilate(res_sell, kernel, iterations=3)
     res_buy = cv2.erode(res_buy, kernel, iterations=2)
     res_sell = cv2.dilate(res_sell, kernel, iterations=3)
@@ -46,3 +43,16 @@ while (True):
 
     # Mostro il pannello
     cv2.imshow("frame", fin)
+
+    # Se applicando la maschera restano degli elementi (=> esistono delle aree verdi), sum sarà > 0
+    hasGreen = np.sum(mask_buy)
+
+    hasRed = np.sum(mask_sell)
+    if hasRed > 0:
+        print("Sell")
+
+    #-- Demo: segnale sonoro
+    duration = 1000  # millisecondi
+    freq = 440  # Hz
+    # winsound.Beep(freq, duration)
+    #--
